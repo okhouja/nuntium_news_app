3a86df551df9869e574765091c07438319cded64
Omar <omar0940@gmail.com>
2021-10-11 11:24:38 +0200

    upload logos

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Home from "./components/Home";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo_lightmode from "../../public/Logo_lightmode.svg";
+function Home() {
+  return (
+    <div>
+      <img src={logo_lightmode} alt="" />
+    </div>
+  );
+}
+
+export default Home;
