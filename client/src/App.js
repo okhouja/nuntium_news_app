9c4fc691c94b21342530ac442926a67a34111cc0
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-10-12 21:23:55 +0200

    search

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+
 function App() {
   return (
     <div>
       <Home />
      <Header /> 
+     <SearchBar/>
     </div>
   );
 }
---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+ function SearchBar  () {
+    return (
+        <div>
+             <div className="search-bar">
+                  <ul className="ul-search-bar">
+                      <li className="li-search-bar"><a href="#">Latest News</a><span className="overlay">
+                      <span><a href="#"></a></span></span></li>
+                      <li className="li-search-bar"><a href="#">General</a><span className="overlay">
+                      <span><a href="#"></a></span></span></li>
+                      <li className="li-search-bar"><a href="#">Busniess</a><span className="overlay">
+                      <span><a href="#"></a></span></span></li>
+                      <li className="li-search-bar"><a href="#">Entertainment</a><span className="overlay">
+                      <span><a href="#"></a></span></span></li>
+                      <li className="li-search-bar"><a href="#">Health</a><span className="overlay">
+                      <span><a href="#"></a></span></span></li>
+                      <li className="li-search-bar"><a href="#">Science</a><span className="overlay">
+                      <span><a href="#"></a></span></span></li>
+                      <li className="li-search-bar"><a href="#">Sports</a><span className="overlay">
+                      <span><a href="#"></a></span></span></li>
+                      <li className="li-search-bar"><a href="#">Technology</a><span className="overlay">
+                      <span><a href="#"></a></span></span></li>
+                      <li className="li-search-bar"><a href="#">More</a><span className="overlay">
+                      <span><a href="#"></a></span></span></li>
+                  
+                  </ul>
+
+
+                </div>
+            
+        </div>
+    )
+};
+export default SearchBar;
