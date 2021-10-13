a30c49de3145ef1edc8cf829061e562eef28ed51
Omar <omar0940@gmail.com>
2021-10-13 13:23:26 +0200

    user_model strucre

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,16 @@
 const UserData = require("../models/userModel");
+
+const userController = {};
+
+// Add new user
+userController.addNewUser = async (req, res) => {
+  const user = new UserData({
+    userName: req.body.userName.toLowerCase(),
+    userPass: req.body.userPass,
+    email: req.body.email,
+    country: req.body.country,
+    city: req.body.city,
+  });
+};
+
+// GET All Users
