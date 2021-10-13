7e1e38dcd66d0b945ea1c23d5dc2b94b9c00b210
Omar <omar0940@gmail.com>
2021-10-13 08:50:24 +0200

    Connect mongoDB

--- server/app.js
+++ server/app.js
@@ -3,6 +3,20 @@ const app = express();
 const morgan = require("morgan");
 app.use(morgan("dev"));
 
-app.route("./").get;
+// Connect to MongoDB
+
+const mongoose = require("mongoose");
+const uri = process.env.Mongo_DB;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((error) => {
+    console.log(`There is a problem ${error.message}`);
+  });
+
+app.route("/").get;
 
 module.exports = app;
