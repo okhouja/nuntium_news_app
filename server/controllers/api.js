b3c0fcd840ba6c69c2ce53c5b183c61f77284c9e
Omar <omar0940@gmail.com>
2021-10-15 21:54:22 +0200

    try to fetch data with axios in BE

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -3,3 +3,7 @@ const APP_KEY = require("../config");
 const axios = require("axios");
 const config = require("./config");
 const APP_URL = `http://api.mediastack.com/v1/news?access_key=${APP_KEY}`;
+
+const news = async () => {
+  const myKey = config.APP_KEY;
+};
