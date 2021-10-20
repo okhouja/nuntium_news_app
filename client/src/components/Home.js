4fc35572ae35c3aa89d3ce000711e5c3fbc376f6
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-10-20 12:18:23 +0200

    search bar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../components/Header";
+import General from "../components/General";
+import Navbar from "../components/Navbar";
+import Searchbar from "../components/SearchBar";
 function Home() {
   return (
     <div>
+      <Header />
+      <Navbar />
+      <Searchbar />
     
     </div>
   );
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,23 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Store, StoreContext } from "../context";
 import axios from "axios";
 import myKey from "../context/config";
 const API_KEY = myKey.apiKey;
 
-
 function SearchBar() {
-    const contextObj = useContext(StoreContext);
-    console.log(contextObj);
-    // Store.fetchData();
-
-
+   
     const [userInput, setUserInput] = useState(false);
-    // const handelChange = () => setUserInput(!userInput);
-    // const  [userInput , setUserInput] = useState(false);
-    // const [categories, setCategories] = useState("general");
-    // const [lang, setLang] = useState("en");
-    // const [countries, setCountries] = useState("");
-    // const [dropdown, setDropdown] = useState({ cat: false, lang: false, countries: false });
+    const handelChange = ()=> setUserInput(!userInput);
+  
 
     // useEffect(() => {
     //     axios.get(`http://api.mediastack.com/v1/news?access_key=${API_KEY}&limit=50&categories=${categories}&languages=${lang}&countries=${countries}&keywords=corona&offset=4`).then(response=>console.log(response.data));
@@ -25,25 +15,31 @@ function SearchBar() {
 
     return (
         <div>
+            <div className="search-bar">
 
 <div>
-<ul className="ul-header">
-    <li className="li-header"><a  href="#"/>General</li>
-    <li className="li-header<"><a href="#"/>Bunsiness</li>
-    <li className="li-header"><a href="#"/>Entertainment</li>
-    <li className="li-header"><a href="#"/>Health</li>
-    <li className="li-header"><a href="#"/>Science</li>
-    <li className="li-header"><a href="#"/>Sports</li>
-    <li className="li-header"><a href="#"/>Technology</li>
+<ul className="ul-search-bar">
+    <li className="search-li"><a  href="#"/>General</li>
+    <li className="search-li"><a href="#"/>Bunsiness</li>
+    <li className="search-li"><a href="#"/>Entertainment</li>
+    <li className="search-li"><a href="#"/>Health</li>
+    <li className="search-li"><a href="#"/>Science</li>
+    <li className="search-li"><a href="#"/>Sports</li>
+    <li className="search-li"><a href="#"/>Technology</li>
+    <li className="searchClick" onClick={handelChange}>🔍</li>
+
 </ul>
+
+
                     </div>
 
-<div className="icon"/>
+<div />
                 
-                <div className={userInput ? "search-div-active" : "search - div"}>
-        < input className = "search-input" type = "text" placeholder = "Type here" /> <button className="search-click">search</button>
+                <div className={userInput ? "search-div-active" : "search-div" }> 
+        < input className = "search-input" type = "text" placeholder = "Type here" /> <button className="search-button">search</button>
                 </div >
             </div >
+            </div>
         
     )
 };
