e4857981980bbf71eb404650ba347b6f0b626074
Omar <omar0940@gmail.com>
2021-10-20 08:26:58 +0200

    comment model

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -6,5 +6,28 @@ const CommentSchema = new mongoose.Schema({
     type: String,
     required: "You need to write your comment",
   },
-  likes: [{ type: mongoose.Schema.ObjectId, ref: "user" }],
+  likes: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+  reply: [
+    {
+      text: String,
+      created: {
+        type: Date,
+        default: Date.now,
+      },
+      repliedBy: {
+        type: mongoose.Schema.ObjectID,
+        ref: "User",
+      },
+    },
+  ],
+  commentedBy: {
+    type: mongoose.Schema.ObjectID,
+    ref: "User",
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+export default mongoose.model("Comment", CommentSchema);
