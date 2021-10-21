f51fbad00d69242a58e5ce0c7dfc8d523d89c781
Omar <omar0940@gmail.com>
2021-10-21 08:09:26 +0200

    user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,43 +1,30 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
-
-// const reqStrMax = {
-//   type: String,
-//   required: [true, "You need "],
-// };
 const UserSchema = new Schema(
-
-
-const UserSchema = new mongoose.Schema(
-
   {
-    _id: mongoose.Types.ObjectId,
+    _id: Schema.Types.ObjectId,
     username: {
       type: String,
       required: [true, "You need to enter a Username"],
       unique: true,
       trim: true,
-      max: [30, "Your username is too long, Max is 30 characters"],
     },
     password: {
       type: String,
       required: [true, "You need to enter a Password"],
-      max: [50, "Your Password is too long."],
     },
     email: {
       type: String,
       require: [true, "You need to enter a Email Address"],
       trim: true,
       unique: "Email already exists",
-      match: [/.+\@.+\..+/, "Please enter a valid Email Address"],
+      match: [/.+\@.+\..+/, "Please fill a valid email address"],
     },
     country: String,
     city: {
       type: String,
       required: [true, "You need to enter your City"],
       trim: true,
-      max: [30, "Your City name is too long."],
     },
     general: Boolean,
     business: Boolean,
