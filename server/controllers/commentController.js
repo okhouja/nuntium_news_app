4f0cd1392b4db05e166b3379c2b64b8d75eb516e
Omar <omar0940@gmail.com>
2021-10-25 15:04:15 +0200

    add comment controller

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,4 +1,4 @@
-const CommentData = require("../models/commentModel");
+const Comment = require("../models/commentModel");
 const User = require("../models/userModel");
 const mongoose = require("mongoose");
 
@@ -7,7 +7,7 @@ const commentCont = {};
 // Check user
 
 commentCont.checkUser = async (req, res, next) => {
-  const user = await User.findById(req.params._id);
+  const user = await User.findById(req.params.id);
   try {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -19,10 +19,38 @@ commentCont.checkUser = async (req, res, next) => {
   next();
 };
 
-commentController.addNewComment = async (req, res) => {
-  const comment = new CommentData({
-    user: req.user.id,
-    text: req.body.text,
+// Get All Comments for All users
+commentCont.getAllComments = async (req, res) => {
+  try {
+    const comment = await Comment.find();
+    res.status(200).json(comment);
+  } catch (err) {
+    res.status(err.status).json({ message: err.message });
+  }
+};
+
+commentCont.addNewComment = async (req, res) => {
+  User.findById(req.params.id).then((user) => {
+    if (user) {
+      const comment = new Comment({
+        _id: new mongoose.Types.ObjectId(),
+        postedBy: req.params.id,
+        content: req.body.content,
+      });
+      comment.save();
+      user.comments.push(comment);
+      user.save();
+      res
+        .status(201)
+        .json({ message: "Your comment has been added successfully" });
+    } else {
+      return res.status(404).json({ message: "User Not Found" });
+    }
+  });
+
+  const comment = new Comment({
+    postedBy: req.params.id,
+    content: req.body.content,
     aricle: req.body.article,
   });
   try {
