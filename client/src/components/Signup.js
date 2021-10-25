a39c082449acf2e2e11b3eed537ca5e57cfe5705
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-10-25 10:40:12 +0200

    sign up

---
+++ client/src/components/Signup.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+
+
+function signup() {
+
+  return <div>
+    <form action="./" method="get">
+      <input type="text" name="username" />
+      <input type="password" name="password" />
+      <input type="password" name="confpassword" />
+      <inpt type="email"name="email"/>
+        <input type="submit" value="Add user" />
+        <input type="reset" value="Clear" />
+
+
+
+    </form>
+  </div>;
+}
+
+export default signup;
