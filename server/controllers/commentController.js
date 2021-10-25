742d2f8bdb4e0447ed86f3f4787a10f8f2129d4a
Omar <omar0940@gmail.com>
2021-10-25 14:32:19 +0200

    checkuser Controller

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,6 +1,23 @@
 const CommentData = require("../models/commentModel");
+const User = require("../models/userModel");
+const mongoose = require("mongoose");
 
 const commentCont = {};
+
+// Check user
+
+commentCont.checkUser = async (req, res, next) => {
+  const user = await User.findById(req.params._id);
+  try {
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (err) {
+    res.status(err.status).json({ message: err.message });
+  }
+  req.author = author;
+  next();
+};
 
 commentController.addNewComment = async (req, res) => {
   const comment = new CommentData({
