b6b083ed8868b1f75a532964187661fd088cdeec
Omar <omar0940@gmail.com>
2021-11-10 15:22:18 +0100

    article Be

--- server/router/article.js
+++ server/router/article.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const articleCont = require("../controllers/articleCtrl");
+const articleCtrl = require("../controllers/articleCtrl");
 
 // URL http://localhost:5000/article
-router.route("/articles").get(articleCont.getAllArticle);
+router.route("/articles").get(articleCtrl.getAllArticle);
 
-router.route("/article/:_id").get(articleCont.getArticle);
+router.route("/article/:_id").get(articleCtrl.getArticle);
 
-router.route("/article/like").post(articleCont.likeArticle);
+router.route("/article/like").post(articleCtrl.likeArticle);
 
 module.exports = router;
