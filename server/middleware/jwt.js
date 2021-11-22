06c9f7b0c0ed2b5b7dc12909f1f90909cb0964a4
Omar <omar0940@gmail.com>
2021-11-22 09:40:40 +0100

    JWT

--- server/middleware/jwt.js
+++ server/middleware/jwt.js
@@ -3,10 +3,17 @@ const { sign, verify } = require("jsonwebtoken");
 const createToken = (user) => {
   const accessToken = sign(
     { username: user.username, id: user._id },
-    process.env.TOKEN_TEXT
+    process.env.TOKEN_TEXT,
+    {
+      // in 600 second = 10 min will be expire
+      expiresIn: 600,
+    }
   );
+
   return accessToken;
 };
-const checkToken = (req, res, next) => {};
+const checkToken = (req, res, next) => {
+  console.log(req.headers.authorization);
+};
 
 module.exports = { createToken, checkToken };
