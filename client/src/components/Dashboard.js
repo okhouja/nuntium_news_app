3a69f2692c6f34cfa9f21ab5ce3d86a47019ae1a
Omar <omar0940@gmail.com>
2021-11-23 14:52:40 +0100

    dashboard

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+function Dashboard() {
+  return <div></div>;
+}
+
+export default Dashboard;
