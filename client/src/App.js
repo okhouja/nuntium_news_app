54959f291dea458594bd3dce2006bba04301d437
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-11-25 13:11:19 +0100

    get info

--- client/src/App.js
+++ client/src/App.js
@@ -15,12 +15,12 @@ import Loading from "./components/Loading";
 import Dashboard from "./components/Dashboard";
 
 
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [load, setLoading] = useState(true);
-
+  const [theme , setTheme] = useState("light");
+  
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
@@ -30,16 +30,14 @@ function App() {
 
   return (
     <Router>
-      <DesktopNavbar />
-      <MobileNavbar />
-      <Dashboard />
-      
-
-      <Header />
+        {/* <Dashboard /> */}
+      <DesktopNavbar  theme={theme} setTheme={setTheme} />
+      <MobileNavbar theme={theme} setTheme={setTheme} />
+      <Header theme={theme} />
       <SearchBar />
 
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route  path="/" exact component={()=><Home theme={theme}/>} />
         <Route path="/signup" exact component={Register} />
         <Route path="/login" exact component={Login} />
         <Route path="/newscollection" exact component={NewsCollection} />
@@ -56,6 +54,9 @@ function App() {
         />
       </Switch>
       <Footer />
+    
+
+
     </Router>
   );
 }
